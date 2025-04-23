26c8d36a2f047ed708d25972421ceb7941462571
alexmjll <alex.melbourne@jll.com>
2025-04-23 17:26:33 +0100

    tokens hooked up

--- token-project/src/button.tsx
+++ token-project/src/button.tsx
@@ -1,19 +1,29 @@
 import Button from '@mui/material/Button';
 import styled from '@emotion/styled';
-import colors from './tokens'
+import allTokkens from './tokens'
 
-const token = {
-    buttonbgprimary: colors.primaryAction[100]
+const localTokens = {
+    buttonbgprimary: allTokkens.primary[100],
+    fontcolor: allTokkens.onprimary[100],
+    radius: allTokkens.radiusfull[100],
+    paddingVertical: allTokkens.paddingV[100],
+    paddingHorizonal: allTokkens.paddingH[100],
+
+
 }
 
 const StyledButton = styled(Button) `
     &.MuiButtonBase-root {
-        background-color: ${token.buttonbgprimary};
+        background-color: ${localTokens.buttonbgprimary};
+        color: ${localTokens.fontcolor};
+        border-radius: ${localTokens.radius}px;
+        padding: ${localTokens.paddingVertical}px ${localTokens.paddingHorizonal}px;
+        box-shadow: none;
     }
 `
 
 export default function MainButton() {
-    console.log(colors)
-    console.log(token, 'token')
+    console.log(localTokens, 'token')
+    console.log(allTokkens)
     return (<StyledButton variant="contained">Contained</StyledButton>)
 }
--- token-project/src/tokens.tsx
+++ token-project/src/tokens.tsx
@@ -1,15 +1,21 @@
 import Tokens from '../../tokens.json'
 
-interface Token {
-    [group: string]: {
-      [key: string]: string;
-    };
-}
-  
-const colors: Token = {
-    primaryAction: {
-      100: Tokens['Facet Theme/Light'].Schemes.Primary.value,
+const colors = {
+    primary: {
+      100: Tokens['PoC/Button_PoC'].primary.value
     },
+    onprimary: {
+        100: Tokens['PoC/Button_PoC']['on-primary'].value
+      },
+      radiusfull: {
+        100: Tokens['PoC/Button_PoC']['radius-full'].value
+      },
+      paddingV: {
+        100: Tokens['PoC/Button_PoC']['padding-v'].value
+      },
+      paddingH: {
+        100: Tokens['PoC/Button_PoC']['padding-h'].value
+      }
 }
 
 export default colors
